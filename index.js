9b48709c1177fd7f8071dfda20d7d4cbf38358fb
Tyler Parker <tpbji16@gmail.com>
2020-03-22 19:19:01 +0000

    huh

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const bot = new Discord.Client();
 bot.on('message', (message) => {
     if(message.author.id == '189045589000978432') {
         message.react('🏳️‍🌈');
-        message.react('🇬');
-        message.react('🇦');
-        message.react('🇾');
     }
 });
 
