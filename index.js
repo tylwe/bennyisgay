c8e1af6c9eda7a7a1356013f98c29abfdae510be
Tyler Parker <tpbji16@gmail.com>
2020-03-22 19:17:23 +0000

    +gay

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 bot.on('message', (message) => {
-    if(message.author.id == '189045589000978432' || message.author.id == '320310862994866186') {
+    if(message.author.id == '189045589000978432') {
         message.react('🏳️‍🌈');
+        message.react('🇬');
+        message.react('🇦');
+        message.react('🇾');
     }
 });
 
