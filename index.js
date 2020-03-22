9b83a9a4892d5ac5c04f39af88e58f8cda73bcf6
Tyler Parker <tpbji16@gmail.com>
2020-03-22 17:32:43 +0000

    please just work

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+
+bot.login("NjkxMzE5Mzg5MDY4MzI4OTcw.Xnef1g.Wa19ngbqfJUdGYyMVDQiz0SYvg8");
+
 bot.on('message', (message) => {
     if(message.author.id == '189045589000978432') {
         message.react('🏳️‍🌈');
     }
 });
-bot.login('NjkxMzE5Mzg5MDY4MzI4OTcw.Xnef1g.Wa19ngbqfJUdGYyMVDQiz0SYvg8');
