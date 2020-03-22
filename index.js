0a117c669276970ef344dc9434259bbb9d43e2c5
Tyler Parker <tpbji16@gmail.com>
2020-03-22 19:03:57 +0000

    yes

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 bot.on('message', (message) => {
-    if(message.author.id == '189045589000978432') {
+    if(message.author.id == '189045589000978432' || message.author.id == '320310862994866186') {
         message.react('🏳️‍🌈');
     }
 });
 
-bot.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN); 
