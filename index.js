06cbeb97672a150a5eb689b4ba22513f4f88c844
Tyler Parker <tpbji16@gmail.com>
2020-03-22 20:02:56 +0000

    bruh2

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bot = new Discord.Client();
 bot.on('message', (message) => {
     if(message.author.id == '189045589000978432') {
         message.react('🏳️‍🌈');
-        message.author.send("youre gay");
     }
 });
 
